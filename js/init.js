ebc3cc09ee943b9e4bb2c78c28a500c90e40bac7
luthfi117 <luthfi.nurhakim@gmail.com>
2015-06-07 15:04:51 +0700

    Adding Smooth Scrolling on Page using #id

--- js/init.js
+++ js/init.js
@@ -20,13 +20,35 @@
    });  // end section function
 }); // close out script*/
 
+//THIS IS FOR PARALLAX EFFECT ON PAGE
+
 $('.home').parallax({imageSrc: 'img/header.jpg'});
 $('.bottom-words').parallax({imageSrc: 'img/parallax2.jpg'});
 /* Create HTML5 element for IE */
 document.createElement("section");
 
+//THIS IS FOR SKILL BAR
+
 jQuery('.skillbar').each(function(){
   jQuery(this).find('.skillbar-bar').animate({
     width:jQuery(this).attr('data-percent')
   },2000);
+});
+
+
+//THIS IS FOR SCROLLING THROUGH MENU BAR -- SMOOTH SCROLL EFFECT
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
